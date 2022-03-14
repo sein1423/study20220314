2c1e9136c21333110174fa6e92f5dc20612128ed
hyeonmin <hyeonmin@DESKTOP-EA4P68P>
2022-03-14 16:26:54 +0900

    초기 커밋.

---
+++ study20220314/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+
+namespace study20220314
+{
+    internal class Program
+    {
+
+        static int add(int a, int b)
+        {
+            return a+b;
+        }
+
+        static float add(float a, float b)
+        {
+            return a+b;
+        }
+
+        static void Main(string[] args)
+        {
+            int a,b;
+            float c, d;
+
+
+            Write("정수를 입력하시오 : ");
+            string key = ReadLine();
+            a = int.Parse(key); 
+            key = ReadLine();
+            b = int.Parse(key);
+            Write("소수를 입력하시오 : ");
+            key = ReadLine();
+            c = float.Parse(key);
+            key = ReadLine();
+            d = float.Parse(key);
+            
+            int i = add(a, b);
+            float f = add(c, d);
+            
+            WriteLine($"정수 : {i} , 소수 : {f}");
+        }
+    }
+}
